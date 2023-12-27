ebd30e204022721fd417b48a3b0a0e0a3ce83533
vikalp <avikalp233@gmail.com>
2023-12-27 17:49:19 +0530

    Mongodb and prisma setup, Register functionality

---
+++ pages/api/auth/[...nextauth].ts
@@ -0,0 +1,59 @@
+import NextAuth, { AuthOptions } from "next-auth"
+import { PrismaAdapter } from "@next-auth/prisma-adapter"
+import prisma from "../../../app/libs/prismadb"
+import GithubProvider from "next-auth/providers/github"
+import CredentialsProvider from "next-auth/providers/credentials"
+import bcrypt from "bcrypt"
+
+export const authOptions: AuthOptions = {
+    adapter: PrismaAdapter(prisma),
+    providers: [
+        GithubProvider({
+            clientId: process.env.GITHUB_ID as string,
+            clientSecret: process.env.GITHUB_SECRET as string,
+        }),
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                email: { label: "email", type: "text" },
+                password: {label: "password", type: "text"},
+            },
+            async authorize(credentials){
+                if(!credentials?.email || !credentials?.password)
+                {
+                    throw new Error("Invalid Credentials")
+                }
+
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: credentials.email
+                    }
+                })
+
+                if(!user || !user?.hashedPassword)
+                {
+                    throw new Error("Invalid Credentials")
+                }
+
+                const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword)
+
+                if(!isCorrectPassword)
+                {
+                    throw new Error("Invalid Credentials")
+                }
+
+                return user
+            }
+        })
+    ],
+    pages: {
+        signIn: "/",
+    },
+    debug: process.env.NODE_ENV==="development",
+    session: {
+        strategy: "jwt",
+    },
+    secret: process.env.NEXTAUTH_SECRET
+}
+
+export default NextAuth(authOptions)
