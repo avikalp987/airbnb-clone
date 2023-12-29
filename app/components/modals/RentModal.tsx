48bc72da6de68394949affc7a9f58508e7c36bcc
vikalp <avikalp233@gmail.com>
2023-12-29 16:44:26 +0530

    Listing step 4 (Images upload)

--- app/components/modals/RentModal.tsx
+++ app/components/modals/RentModal.tsx
@@ -10,6 +10,7 @@ import { FieldValues, useForm } from "react-hook-form";
 import CountrySelect from "../inputs/CountrySelect";
 import dynamic from "next/dynamic";
 import Counter from "../inputs/Counter";
+import ImageUpload from "../inputs/ImageUpload";
 
 enum STEPS{
     CATEGORY=0,
@@ -55,6 +56,7 @@ const RentModal = () => {
     const guestCount = watch("guestCount")
     const roomCount = watch("roomCount")
     const bathRoomCount = watch("bathRoomCount")
+    const imageSrc = watch("imageSrc")
 
     const Map = useMemo(()=> dynamic(() => import("../Map"), {
         ssr: false
@@ -171,6 +173,22 @@ const RentModal = () => {
                     subtitle="How many bathrooms do you have?"
                     value={bathRoomCount}
                     onChange={(value)=>setCustomValue("bathRoomCount", value)}
+                />
+            </div>
+        )
+    }
+
+    if(step === STEPS.IMAGES){
+        bodyContent = (
+            <div className="flex flex-col gap-8">
+                <Heading 
+                    title="Add a photo of your place"
+                    subTitle="Show guests what your place looks like!"
+                />
+
+                <ImageUpload
+                    value={imageSrc}
+                    onChange={(value) => setCustomValue("imageSrc", value)}
                 />
             </div>
         )
--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const nextConfig = {
     images: {
         domains: [
             "avatars.githubusercontent.com",
+            "res.cloudinary.com",
             
         ]
     }
